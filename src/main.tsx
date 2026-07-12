050af24d8ba2c6f02732f9858b732796f930a8bd
Codenames-Ren <renmizuki38@gmail.com>
2026-07-12 15:46:01 +0700

    adjust main and add react router dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 import "./styles.css";
-import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
